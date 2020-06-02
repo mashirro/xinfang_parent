ed3fdf84ef57770af6ef563276275092fe6e92fe
chenzg <18627901759@163.com>
2020-06-02 11:55:27 +0800

    测试freemarker用控制器

--- xinfang_system/src/main/java/com/mashirro/xinfang_system/controller/IndexController.java
+++ xinfang_system/src/main/java/com/mashirro/xinfang_system/controller/IndexController.java
@@ -4,6 +4,9 @@ package com.mashirro.xinfang_system.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+/**
+ * 验证配置freemarker是否成功的控制器
+ */
 @Controller
 public class IndexController {
 
